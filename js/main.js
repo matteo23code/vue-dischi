dbbacff0a8fc651f1d4a54d3d0742bed87c120ef
Matteo Basile <matteo.basile56@gmail.com>
2021-02-08 18:10:48 +0100

    es completo

---
+++ js/main.js
@@ -0,0 +1,35 @@
+new Vue({
+  el: '#app',
+  data:{
+    selected: '',
+    playList:[
+
+    ],
+  },
+  methods:{
+    search: function () {
+      console.log("playList", this.playList);
+      this.playList.forEach((item) => {
+        let musicalGenre = item.genre;
+        let searchString = this.selected;
+        console.log(this.selected);
+        if (musicalGenre.includes(searchString)) {
+          item.success = true
+        }else {
+          item.success = false
+        }
+      });
+    }
+  },
+  mounted() {
+    const self = this;
+
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(function(resp) {
+       const disk = resp.data.response
+      self.playList = disk;
+      console.log(self.playList);
+    });
+  }
+});
+Vue.config.devtools = true
